4b32e12c5f116c09cdc8e61426cbe537596eeb2e
ls-yw <405548753@qq.com>
2019-02-14 09:37:07 +0800

    del redis and modules config

--- application/config/config.php
+++ application/config/config.php
@@ -7,8 +7,6 @@
 return new \Phalcon\Config([
     'application' => [
         'database'  => require_once APP_PATH.'/config/database.php',
-        'redis'     => require_once APP_PATH.'/config/redis.php',
-        'modules'   => require_once APP_PATH.'/config/modules.php',
         'logsPath'  => '/data/logs/'.APP_NAME.'/',
         'viewsDir'  => APP_PATH.'/views',
     ]
